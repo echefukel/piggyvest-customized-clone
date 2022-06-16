8e5c5c020f606cf4d8c6286f90f449ac8b63c0d2
Kelechi Obinna Echefu <kellymoor18@gmail.com>
2022-06-16 11:53:30 +0100

    design changes

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const menuIcon = document.querySelector('.menu-icon');
-function myFunction(x){
-    x.classList.toggle('change');
+const menuIcon = document.getElementById('menu-icon');
+const menuItems = document.querySelector('.menu-items1')
+
+
+const switchNav = () =>{
+    menuIcon.classList.toggle('change');
+
+// fade in menu items
+if(menuItems.classList.contains('menu-items1')){
+    menuItems.classList.replace('menu-items1','menu-items2')
+}else{
+    menuItems.classList.replace('menu-items2','menu-items1')
+
 }
+}
+
+
+
+
+
+
+
+
+
+menuIcon.addEventListener('click',switchNav);
